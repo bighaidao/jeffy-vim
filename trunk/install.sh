5ee94032370bafa39d468ea616f8fc0a6ada043a
jeffy.du <jeffy.du@b8ead0c8-bffa-11de-91a4-794fdb80bb57>
2009-10-25 10:02:41 +0000

    Version 1.0

--- trunk/install.sh
+++ trunk/install.sh
@@ -7,17 +7,25 @@ if [[ $answer == "n" ]]; then
 	echo "Install abord !!!"
 	exit -1
 fi
+
 echo -n "Now, install vim files..."
+
+# remove old files
 if [[ -d ~/.vim ]]; then
 	rm -r ~/.vim
 fi
 if [[ -f ~/.vimrc ]]; then
 	rm ~/.vimrc
 fi
-cp -r ./vim ~/.vim
-cp ./vimrc ~/.vimrc
+
+# copy new files
+cp -r ./jeffy-vim ~/.vim
+cp ./jeffy-vimrc ~/.vimrc
+
+# create backup directory
 if [[ ! -d ~/.vim.backupdir ]]; then
 	mkdir ~/.vim.backupdir
 fi
+
 echo "done." 
 echo "Enjoy yourself! :-)"
